9c24d61918ec98c2cf3cccfda88ea1e6d6d04f82
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-05-24 16:50:35 -0500

    hide power by

--- myApp.js
+++ myApp.js
@@ -1,61 +1,26 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
+
 const api = require('./server.js');
+
 app.use(express.static('public'));
+
+app.use(helmet.hidePoweredBy());
+
 app.disable('strict-transport-security');
+
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
